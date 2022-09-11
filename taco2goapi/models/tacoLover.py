9b0dc876ed29be1b14f29fe492155ecd4584fe4d
Duley Williams <duleygrafix@gmail.com>
2022-09-11 12:11:08 -0500

    total_fixture

--- taco2goapi/models/tacoLover.py
+++ taco2goapi/models/tacoLover.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class TacoLover(models.Model):
-    taco_lover = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profilePic = models.URLField(max_length=200)
     userCity = models.CharField(max_length=50)
     email = models.EmailField()
