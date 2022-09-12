63afd31d6734a7aaea496d858c3e455984e7ec39
Duley Williams <duleygrafix@gmail.com>
2022-09-12 18:58:30 -0500

    modelUpdate

--- taco2goapi/models/myBuiltTaco.py
+++ taco2goapi/models/myBuiltTaco.py
@@ -4,6 +4,7 @@ from taco2goapi.models.shell import Shell
 from taco2goapi.models.protein import Protein
 
 class MyBuiltTaco(models.Model):
+    name = models.CharField(max_length=55, default="")
     tacoLoverId = models.ForeignKey(TacoLover, on_delete=models.CASCADE)
     tacoShellId = models.ForeignKey(Shell, on_delete=models.CASCADE)
     tacoProteinId = models.ForeignKey(Protein, on_delete=models.CASCADE)
